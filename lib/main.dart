35294ccdce1e735ca00b63f56a8d90289ee9a272
Ryan Luu <ryan.luu@ryanluu.dev>
2024-04-10 23:18:36 -0700

    Add dynamic_color/dynamic_color.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 import 'screens/home.dart';
 import 'theme.dart';
