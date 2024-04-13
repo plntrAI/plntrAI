06ac8dd78dc8ec7e465e68c6a4cbd43f24b9910c
Ryan Luu <ryan.luu@ryanluu.dev>
2024-04-13 06:57:26 -0700

    Add basic unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:plntrai/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:plntrai/screens/home.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+  final mockHandleBrightnessChange = (bool useLightMode) {};
+  final mockHandleThemeChange = (bool useDynamic) {};
+
+  testWidgets('HomeScreen can be created', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: HomeScreen(
+        useLightMode: true,
+        handleBrightnessChange: mockHandleBrightnessChange,
+        handleThemeChange: mockHandleThemeChange,
+      ),
+    ));
+
+    expect(find.byType(HomeScreen), findsOneWidget);
+  });
+
+  testWidgets('SettingsDialog can be created', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: SettingsDialog(
+          handleBrightnessChange: mockHandleBrightnessChange,
+          handleThemeChange: mockHandleThemeChange,
+        ),
+      ),
+    ));
+
+    expect(find.byType(SettingsDialog), findsOneWidget);
+  });
 }
